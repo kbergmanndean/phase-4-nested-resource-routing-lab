73430b12f96bc1ed21de794e1916db034ad2593f
Kaia <kbergmanndean@gmail.com>
2021-07-21 19:59:30 -0400

    commit tree 1e4efd8b364e177f5632acd2fb3940dcd8ad9f3d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user=User.find(params[:user_id])
+      items=user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  rescue ActiveRecord::RecordNotFound=>not_found
+    render json: {errors:invalid.record.errors},status: :not_found
   end
 
 end
